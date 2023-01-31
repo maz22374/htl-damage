c8dfaf10b784e634741bef85cc47b270adba8b8f
David Mazurek <maz22374@spengergasse.at>
2023-01-31 10:00:33 +0100

    roomCategories and Users

---
+++ htl_damage_app/HtlDamage.Application/Model/RoomCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtlDamage.Application.Model
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class RoomCategory
+    {
+        public RoomCategory(string name)
+        {
+            Name = name;
+        }
+
+#pragma warning disable CS8618 
+        protected RoomCategory() { }
+#pragma warning restore CS8618 
+
+        public int Id { get; private set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+    }
+}
