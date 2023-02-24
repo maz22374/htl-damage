2931ca73a7aa8859d3f6ed05fea797425ae68131
David Mazurek <maz22374@spengergasse.at>
2023-02-24 12:38:43 +0100

    startServer.cmd

--- startServer.cmd
+++ startServer.cmd
@@ -1,11 +1,3 @@
 @echo off
-docker rm -f htldamage_sqlserver2019 2> nul
-docker run -d -p 1433:1433 --name htldamage_sqlserver2019 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SqlServer2019" mcr.microsoft.com/azure-sql-edge
-IF ERRORLEVEL 1 GOTO error
 dotnet build htl_damage_app/Htldamage.Webapi --no-incremental --force
 dotnet watch run -c Debug --project htl_damage_app/htldamage.Webapi
-GOTO end
-
-:error
-PAUSE
-:end
--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,2 @@
-docker rm -f htldamage_sqlserver2019 &> /dev/nul
-docker run -d -p 1433:1433 --name htldamage_sqlserver2019 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SqlServer2019" mcr.microsoft.com/azure-sql-edge
 dotnet build htl_damage_app/Htldamage.Webapi --no-incremental --force
 dotnet watch run -c Debug --project htl_damage_app/htldamage.Webapi
